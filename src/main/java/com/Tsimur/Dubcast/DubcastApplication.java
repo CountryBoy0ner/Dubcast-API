ba7e3e09faf622a641191d1dd6029eee2f032d10
CountryBoy0ner <suzukieoner@mail.ru>
2025-10-17 11:20:42 +0300

    chore: initial local skeleton

---
+++ src/main/java/com/Tsimur/Dubcast/DubcastApplication.java
@@ -0,0 +1,11 @@
+package com.Tsimur.Dubcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubcastApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DubcastApplication.class, args);
+	}
+}
---
+++ src/test/java/com/Tsimur/Dubcast/DubcastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tsimur.Dubcast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubcastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
